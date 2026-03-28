635a5ae3cafc3ec74cd23c60e14f3bd03a49fc56
Bis-Quit <rifqy8620@gmail.com>
2026-03-28 23:31:55 +0700

    feat: implement CardData ScriptableObject and add 24 ability cards
    - Membuat script CardData.cs sebagai template Scriptable Object untuk fondasi sistem drafting kartu.
    - Menambahkan struktur data kompleks untuk mendukung mekanik kartu yang dinamis.
    - Melakukan data entry dan membuat 24 aset kartu awal, terbagi dalam kategori Instant dan Permanent.

---
+++ Assets/Core/Scripts/Data/CardData.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "ShibuyaRoboFight/Card Data")]
+
+public class CardData : ScriptableObject
+{
+    [Header("Card Idetity")]
+    [SerializeField] private string cardID;
+    [SerializeField] private string cardName;
+
+    [TextArea(2, 4)]
+    [SerializeField] private string description;
+    [SerializeField] private Sprite cardIllustration;
+
+    [Header("Attribute")]
+    [SerializeField] private int abilityPointCost;
+    [SerializeField] private CardCategory cardCategory;
+
+    [Header("Buzz Tile System")]
+    [SerializeField] private bool produceBuzzTile;
+    [SerializeField] private string buzzTileID;
+
+    [Header("Player Condition")]
+    [SerializeField] private TargetSubject conditionSubject;
+    [SerializeField] private ConditionTrigger conditionType;
+    [SerializeField] private OperativeAction operativeCondition;
+    [SerializeField] private GameState conditionState;
+    [SerializeField] private Comprative comprativeCondition;
+    [SerializeField] private int conditionValue;
+
+    [Header("Effect")]
+    [SerializeField] private TargetSubject effectTarget;
+    [SerializeField] private EffectAction effectType;
+    [SerializeField] private GameState targetState;
+    [SerializeField] private int effectValue;
+
+    public enum CardCategory { Instant, Permanent }
+    public enum TargetSubject { Self, Opponent }
+    public enum ConditionTrigger { Immediately, ForEach, If }
+    public enum OperativeAction { Has, Add, Subtract, Start }
+    public enum Comprative { Equals, MoreThan }
+    public enum EffectAction { Add, Subtract }
+
+    public enum GameState
+    {
+        AbilityCard,
+        AbilityPoint,
+        HealthPoint,
+        Fame,
+        Destruction,
+        Turn,
+        Dice
+    }
+}
